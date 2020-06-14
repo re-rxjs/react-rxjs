e59b6028b9a51fea8831eb35f4e3e511e645b614
Josep M Sobrepere <josepot@gmail.com>
2020-06-15 01:36:17 +0200

    improving tests a bit

--- test/operators/distinct-share-replay.test.ts
+++ test/operators/distinct-share-replay.test.ts
@@ -1,6 +1,8 @@
-import { distinctShareReplay } from "../../src"
+import { BehaviorObservable, distinctShareReplay, SUSPENSE } from "../../src"
+import { EMPTY_VALUE } from "../../src/operators/distinct-share-replay"
 import { cold } from "jest-marbles"
 import { TestScheduler } from "rxjs/testing"
+import { Subject } from "rxjs"
 
 const scheduler = () =>
   new TestScheduler((actual, expected) => {
@@ -71,6 +73,58 @@ describe("operators/distinctShareReplay", () => {
       expectObservable(shared, sub1).toBe(expected1);
       expectObservable(shared, sub2).toBe(expected2);
       expectSubscriptions(source.subscriptions).toBe(sourceSubs);
+    })
+  })
+
+  describe("Returns a BehaviorObservable which exposes a getValue function", () => {
+    it("getValue returns the latest emitted value", () => {
+      const input = new Subject<string>()
+      const obs$ = input.pipe(distinctShareReplay()) as BehaviorObservable<
+        string
+      >
+
+      const subscription = obs$.subscribe()
+
+      input.next("foo")
+      expect(obs$.getValue()).toBe("foo")
+
+      input.next("bar")
+      expect(obs$.getValue()).toBe("bar")
+
+      subscription.unsubscribe()
+    })
+
+    it("getValue throws EMPTY_VALUE if nothing has been emitted", () => {
+      const input = new Subject<string>()
+      const obs$ = input.pipe(distinctShareReplay()) as BehaviorObservable<
+        string
+      >
+
+      const subscription = obs$.subscribe()
+      let error: any
+      try {
+        obs$.getValue()
+      } catch (e) {
+        error = e
+      }
+      expect(error).toBe(EMPTY_VALUE)
+      subscription.unsubscribe()
+    })
+
+    it("getValue throws SUSPENSE if the latest emitted value is SUSPENSE", () => {
+      const input = new Subject<any>()
+      const obs$ = input.pipe(distinctShareReplay()) as BehaviorObservable<any>
+
+      const subscription = obs$.subscribe()
+      input.next(SUSPENSE)
+      let error: any
+      try {
+        obs$.getValue()
+      } catch (e) {
+        error = e
+      }
+      expect(error).toBe(SUSPENSE)
+      subscription.unsubscribe()
     })
   })
 })
--- test/operators/react-enhancer.test.ts
+++ test/operators/react-enhancer.test.ts
@@ -1,5 +1,7 @@
 import reactEnhancer from "../../src/operators/react-enhancer"
+import { BehaviorObservable, distinctShareReplay, SUSPENSE } from "../../src"
 import { TestScheduler } from "rxjs/testing"
+import { Subject } from "rxjs"
 
 const scheduler = () =>
   new TestScheduler((actual, expected) => {
@@ -64,6 +66,74 @@ describe("operators/reactEnhancer", () => {
       expectObservable(shared, sub1).toBe(expected1)
       expectObservable(shared, sub2).toBe(expected2)
       expectSubscriptions(source.subscriptions).toBe(sourceSubs)
+    })
+  })
+
+  describe("Returns a BehaviorObservable which exposes a getValue function", () => {
+    it("getValue returns the latest emitted value", () => {
+      const input = new Subject<string>()
+      const obs$ = reactEnhancer(
+        input.pipe(distinctShareReplay()),
+        0,
+      ) as BehaviorObservable<string>
+
+      const subscription = obs$.subscribe()
+
+      input.next("foo")
+      expect(obs$.getValue()).toBe("foo")
+
+      input.next("bar")
+      expect(obs$.getValue()).toBe("bar")
+
+      subscription.unsubscribe()
+    })
+
+    it("if nothing has been emitted, then getValue throws a promise that will resolve when the first value is emitted", async () => {
+      const input = new Subject<string>()
+      const obs$ = reactEnhancer(
+        input.pipe(distinctShareReplay()),
+        0,
+      ) as BehaviorObservable<string>
+
+      const subscription = obs$.subscribe()
+      let error: any
+      try {
+        obs$.getValue()
+      } catch (e) {
+        error = e
+      }
+
+      expect(error).toBeInstanceOf(Promise)
+
+      setTimeout(() => input.next("foo"), 10)
+      await expect(error).resolves.toBe("foo")
+
+      subscription.unsubscribe()
+    })
+
+    it("if the latest emitted value is SUSPENSE, then getValue throws a promise that will resolve when the next non SUSPENSE value is emitted", async () => {
+      const input = new Subject<any>()
+      const obs$ = reactEnhancer(
+        input.pipe(distinctShareReplay()),
+        0,
+      ) as BehaviorObservable<any>
+
+      const subscription = obs$.subscribe()
+      let error: any
+      try {
+        obs$.getValue()
+      } catch (e) {
+        error = e
+      }
+
+      expect(error).toBeInstanceOf(Promise)
+
+      setTimeout(() => input.next(SUSPENSE), 10)
+      setTimeout(() => input.next(SUSPENSE), 20)
+      setTimeout(() => input.next("bar"), 30)
+      await expect(error).resolves.toBe("bar")
+
+      subscription.unsubscribe()
     })
   })
 })
