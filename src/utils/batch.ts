725ab1d479ef9f84ab93a6934e08131b41110b68
Josep M Sobrepere <josepot@gmail.com>
2020-06-16 14:31:22 +0200

    Remove React-(dom/native) unstable_batchedUpdates

--- src/utils/batch.ts
+++
@@ -1,8 +0,0 @@
-function defaultNoopBatch(callback: () => void) {
-  callback()
-}
-
-let batch = defaultNoopBatch
-
-export const setBatch = (newBatch: () => void) => (batch = newBatch)
-export const getBatch = () => batch
