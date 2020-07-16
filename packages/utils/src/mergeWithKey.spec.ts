dd6c4d06cf92ae73cc48b934d6a3bc86cca3640b
Josep M Sobrepere <josepot@gmail.com>
2020-07-17 01:15:39 +0200

    test(mergeWithKey) import operator from index

--- packages/utils/src/mergeWithKey.spec.ts
+++ packages/utils/src/mergeWithKey.spec.ts
@@ -1,6 +1,6 @@
 import { map } from "rxjs/operators"
 import { TestScheduler } from "rxjs/testing"
-import { mergeWithKey } from "./mergeWithKey"
+import { mergeWithKey } from "./"
 
 const scheduler = () =>
   new TestScheduler((actual, expected) => {
@@ -10,16 +10,14 @@ const scheduler = () =>
 describe("mergeWithKey", () => {
   it("emits the key of the stream that emitted the value", () => {
     scheduler().run(({ expectObservable, cold }) => {
-      const sourceA = cold("a---b---|");
+      const sourceA = cold("a---b---|")
       const sourceB = cold("-1--2----3|")
       const expected = "    mn--(op)-q|"
 
       const result = mergeWithKey({
         strings: sourceA,
-        numbers: sourceB
-      }).pipe(
-        map(({ type, payload }) => `${type}:${payload}`),
-      )
+        numbers: sourceB,
+      }).pipe(map(({ type, payload }) => `${type}:${payload}`))
 
       expectObservable(result).toBe(expected, {
         m: "strings:a",
@@ -31,4 +29,3 @@ describe("mergeWithKey", () => {
     })
   })
 })
-
