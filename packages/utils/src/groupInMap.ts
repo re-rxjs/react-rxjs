ac2ba2c0652bf0b2c22ea79153a9af6776d3c9b0
Josep M Sobrepere <josepot@gmail.com>
2020-07-17 01:14:52 +0200

    fix(groupInMap): just emit empty map on source completion

--- packages/utils/src/groupInMap.ts
+++ packages/utils/src/groupInMap.ts
@@ -1,5 +1,12 @@
-import { Observable, GroupedObservable, concat, of } from "rxjs"
-import { map, mergeMap, scan } from "rxjs/operators"
+import { Observable, GroupedObservable, concat, of, defer } from "rxjs"
+import {
+  map,
+  mergeMap,
+  scan,
+  publish,
+  takeUntil,
+  takeLast,
+} from "rxjs/operators"
 import continuousGroupBy from "./continuousGroupBy"
 
 const DELETE = Symbol("DELETE")
@@ -7,25 +14,39 @@ const DELETE = Symbol("DELETE")
 /**
  * Groups all values by key and emits a Map that hold the latest value for each
  * key.
- * 
+ *
  * @param keyGetter Key getter.
  * @param projection Projection function for each group.
  */
 export const groupInMap = <T, K, V>(
   keyGetter: (x: T) => K,
   projection: (x: GroupedObservable<K, T>) => Observable<V>,
-) => (source$: Observable<T>): Observable<Map<K, V>> =>
-  source$.pipe(
-    continuousGroupBy(keyGetter),
-    mergeMap(inner$ =>
-      concat(
-        projection(inner$).pipe(map(v => [inner$.key, v] as const)),
-        of([inner$.key, DELETE] as const),
-      ),
+) => (source$: Observable<T>): Observable<Map<K, V>> => {
+  const res = new Map<K, V>()
+
+  return concat(
+    source$.pipe(
+      continuousGroupBy(keyGetter),
+      publish((multicasted$) => {
+        return multicasted$.pipe(
+          mergeMap((inner$) =>
+            concat(
+              projection(inner$).pipe(map((v) => [inner$.key, v] as const)),
+              of([inner$.key, DELETE] as const),
+            ),
+          ),
+          takeUntil(multicasted$.pipe(takeLast(1))),
+        )
+      }),
+      scan((acc, [key, value]) => {
+        if (value !== DELETE) return acc.set(key, value)
+        acc.delete(key)
+        return acc
+      }, res),
     ),
-    scan((acc, [key, value]) => {
-      if (value !== DELETE) return acc.set(key, value)
-      acc.delete(key)
-      return acc
-    }, new Map<K, V>()),
+    defer(() => {
+      res.clear()
+      return of(res)
+    }),
   )
+}
