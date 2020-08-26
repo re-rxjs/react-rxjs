e9287c34d806ae80cfafb802b30d42406e3da754
Josep M Sobrepere <josepot@gmail.com>
2020-08-26 19:30:07 +0200

    chore(utils): collector abstraction to reduce bundle size

--- packages/utils/src/internal-utils.ts
+++ packages/utils/src/internal-utils.ts
@@ -1,5 +1,13 @@
-import { Observable, defer } from "rxjs"
-import { scan } from "rxjs/operators"
+import { Observable, defer, GroupedObservable } from "rxjs"
+import { shareLatest } from "@react-rxjs/core"
+import {
+  scan,
+  publish,
+  endWith,
+  takeLast,
+  takeUntil,
+  mergeMap,
+} from "rxjs/operators"
 
 export const defaultStart = <T>(value: T) => (source$: Observable<T>) =>
   new Observable<T>((observer) => {
@@ -28,3 +36,32 @@ export const scanWithDefaultValue = <I, O>(
     const seed = getSeed()
     return source.pipe(scan(accumulator, seed), defaultStart(seed))
   })
+
+export const set = "s" as const
+export const del = "d" as const
+export const complete = "c" as const
+
+export const collector = <K, V, VV>(
+  source: Observable<GroupedObservable<K, V>>,
+  enhancer: (
+    source: GroupedObservable<K, V>,
+  ) => Observable<{ t: "d"; k: K } | { t: "s"; k: K; v: VV }>,
+): Observable<Map<K, VV>> =>
+  source.pipe(
+    publish((x) => x.pipe(mergeMap(enhancer), takeUntil(takeLast(1)(x)))),
+    endWith({ t: complete }),
+    scanWithDefaultValue(
+      (acc, val) => {
+        if (val.t === set) {
+          acc.set(val.k, val.v)
+        } else if (val.t === del) {
+          acc.delete(val.k)
+        } else {
+          acc.clear()
+        }
+        return acc
+      },
+      () => new Map<K, VV>(),
+    ),
+    shareLatest(),
+  )
