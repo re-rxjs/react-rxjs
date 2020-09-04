97cb9b0486bb15ee81541355e9dceda723fd2f43
Josep M Sobrepere <josepot@gmail.com>
2020-09-04 11:48:49 +0200

    feat: new API for useSubscribe & Subscribe
    Co-authored-by: Victor Oliva <olivarra1@gmail.com>

--- packages/core/src/Subscribe.tsx
+++ packages/core/src/Subscribe.tsx
@@ -1,5 +1,4 @@
-import React from "react"
-import { useSubscribe } from "./useSubscribe"
+import React, { useState, useEffect } from "react"
 import { Observable } from "rxjs"
 
 /**
@@ -7,12 +6,19 @@ import { Observable } from "rxjs"
  * the component mounts and it unsubscribes when the component unmounts.
  *
  * @param source$ Source observable that the Component will subscribe to.
+ * @param fallback (=null) JSX Element to be rendered before the subscription exists.
  *
  * @remarks This Component doesn't trigger any updates.
  */
 export const Subscribe: React.FC<{
   source$: Observable<any>
-}> = ({ source$, children }) => {
-  useSubscribe(source$)
-  return <>{children}</>
+  fallback?: null | JSX.Element
+}> = ({ source$, children, fallback }) => {
+  const [mounted, setMounted] = useState(0)
+  useEffect(() => {
+    const subscription = source$.subscribe()
+    setMounted(1)
+    return () => subscription.unsubscribe()
+  }, [source$])
+  return <>{mounted ? children : fallback}</>
 }
