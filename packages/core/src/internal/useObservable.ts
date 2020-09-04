b943ed840830f8d0d83896d65d3d5aaff86f07d5
Josep M Sobrepere <josepot@gmail.com>
2020-09-04 11:29:27 +0200

    chore(useObservable): small perf improvements

--- packages/core/src/internal/useObservable.ts
+++ packages/core/src/internal/useObservable.ts
@@ -1,10 +1,10 @@
 import { useEffect, useReducer } from "react"
 import { BehaviorObservable } from "./BehaviorObservable"
 import { SUSPENSE } from "../SUSPENSE"
+import { Observable } from "rxjs"
 
 const ERROR: "e" = "e"
 const VALUE: "v" = "v"
-const SUSP: "s" = "s"
 type Action = "e" | "v" | "s"
 
 const reducer = (
@@ -17,15 +17,31 @@ const reducer = (
 
 const init = (source$: BehaviorObservable<any>) => source$.getValue()
 
+const defaultSUSPENSE = <T>(source$: Observable<T>) =>
+  new Observable<T | typeof SUSPENSE>((observer) => {
+    let isEmpty = true
+    const subscription = source$.subscribe(
+      (x) => {
+        isEmpty = false
+        observer.next(x)
+      },
+      (e) => observer.error(e),
+    )
+
+    if (isEmpty) {
+      observer.next(SUSPENSE)
+    }
+
+    return subscription
+  })
+
 export const useObservable = <O>(
   source$: BehaviorObservable<O>,
 ): Exclude<O, typeof SUSPENSE> => {
   const [state, dispatch] = useReducer(reducer, source$, init)
-  if (state.type === ERROR) throw state.payload
 
   useEffect(() => {
-    dispatch(source$.getValue())
-    const subscription = source$.subscribe(
+    const subscription = defaultSUSPENSE(source$).subscribe(
       (value) => {
         if ((value as any) === SUSPENSE) {
           dispatch(source$.getValue())
@@ -45,8 +61,7 @@ export const useObservable = <O>(
     return () => subscription.unsubscribe()
   }, [source$])
 
-  if (state.type === SUSP) {
-    throw state.payload
-  }
-  return state.payload
+  const { type, payload } = state
+  if (type === VALUE) return payload
+  throw payload
 }
