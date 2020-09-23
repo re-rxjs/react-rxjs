03ff963794fa029ef3fd3b7798007dca4ddfc4e8
Josep M Sobrepere <josepot@gmail.com>
2020-09-23 10:55:15 +0200

    fix(utils): fix createListener typings

--- packages/utils/src/createListener.ts
+++ packages/utils/src/createListener.ts
@@ -5,10 +5,8 @@ const defaultMapper: any = (v: unknown) => v
 export function createListener<A extends unknown[], T>(
   mapper: (...args: A) => T,
 ): [Observable<T>, (...args: A) => void]
-export function createListener<T = void>(): [
-  Observable<T>,
-  (payload: T) => void,
-]
+export function createListener(): [Observable<void>, () => void]
+export function createListener<T>(): [Observable<T>, (payload: T) => void]
 
 export function createListener<A extends unknown[], T>(
   mapper: (...args: A) => T = defaultMapper,
