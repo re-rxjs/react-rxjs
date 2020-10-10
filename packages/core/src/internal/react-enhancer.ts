8ea930d4f240f5e052aecd94e42d3728e3c3cd40
Josep M Sobrepere <josepot@gmail.com>
2020-10-10 04:40:20 +0200

    perf(core): lots of performance improvements

--- packages/core/src/internal/react-enhancer.ts
+++ packages/core/src/internal/react-enhancer.ts
@@ -1,81 +1,59 @@
 import { Observable } from "rxjs"
 import { SUSPENSE } from "../SUSPENSE"
-import { BehaviorObservable, Action } from "./BehaviorObservable"
+import { BehaviorObservable } from "./BehaviorObservable"
 import { EMPTY_VALUE } from "./empty-value"
 
 const reactEnhancer = <T>(source$: Observable<T>): BehaviorObservable<T> => {
-  const result = new Observable<T>((subscriber) => {
-    let latestValue = EMPTY_VALUE
-    return source$.subscribe(
-      (value) => {
-        if (!Object.is(latestValue, value)) {
-          subscriber.next((latestValue = value))
-        }
-      },
-      (e) => {
-        subscriber.error(e)
-      },
-    )
-  }) as BehaviorObservable<T>
+  const result = new Observable<T>((subscriber) =>
+    source$.subscribe(subscriber),
+  ) as BehaviorObservable<T>
 
-  let promise: undefined | { type: Action.Suspense; payload: Promise<T | void> }
-  let error:
-    | typeof EMPTY_VALUE
-    | { type: Action.Error; payload: any } = EMPTY_VALUE
-  const getValue = (): { type: Action; payload: any } => {
+  let promise: Promise<T | void> | undefined
+  let error: any = EMPTY_VALUE
+  const getValue = (): T => {
     let timeoutToken
     if (error !== EMPTY_VALUE) {
       clearTimeout(timeoutToken)
       timeoutToken = setTimeout(() => {
         error = EMPTY_VALUE
       }, 50)
-      return error
+      throw error
     }
 
     try {
-      return {
-        type: Action.Value,
-        payload: (source$ as BehaviorObservable<T>).getValue(),
-      }
+      return (source$ as BehaviorObservable<T>).getValue()
     } catch (e) {
-      if (promise) return promise
+      if (promise) throw promise
 
-      let value:
-        | typeof EMPTY_VALUE
-        | { type: Action.Value; payload: T } = EMPTY_VALUE
+      let value: typeof EMPTY_VALUE | T = EMPTY_VALUE
 
-      promise = {
-        type: Action.Suspense,
-        payload: new Promise<T>((res) => {
-          const subscription = result.subscribe(
-            (v) => {
-              if (v !== (SUSPENSE as any)) {
-                value = { type: Action.Value, payload: v }
-                subscription && subscription.unsubscribe()
-                res(v)
-              }
-            },
-            (e) => {
-              error = { type: Action.Error, payload: e }
-              timeoutToken = setTimeout(() => {
-                error = EMPTY_VALUE
-              }, 50)
-              res()
-            },
-          )
-          if (value !== EMPTY_VALUE) {
-            subscription.unsubscribe()
-          }
-        }).finally(() => {
-          promise = undefined
-        }),
-      }
+      promise = new Promise<T>((res) => {
+        const subscription = result.subscribe(
+          (v) => {
+            if (v !== (SUSPENSE as any)) {
+              value = v
+              subscription && subscription.unsubscribe()
+              res(v)
+            }
+          },
+          (e) => {
+            error = e
+            timeoutToken = setTimeout(() => {
+              error = EMPTY_VALUE
+            }, 50)
+            res()
+          },
+        )
+        if (value !== EMPTY_VALUE) {
+          subscription.unsubscribe()
+        }
+      }).finally(() => {
+        promise = undefined
+      })
 
-      if (value !== EMPTY_VALUE) {
-        return value
-      }
+      if (value !== EMPTY_VALUE) return value
 
-      return error !== EMPTY_VALUE ? error : promise
+      throw error !== EMPTY_VALUE ? error : promise
     }
   }
   result.getValue = getValue
