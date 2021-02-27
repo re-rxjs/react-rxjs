799c246c6440ec0aa2105f61be18fbd550cf43f0
Josep M Sobrepere <josepot@gmail.com>
2021-02-27 08:56:59 +0100

    feat(utils): getGroupedObservable

--- packages/utils/src/index.tsx
+++ packages/utils/src/index.tsx
@@ -1,5 +1,6 @@
 export { collectValues } from "./collectValues"
 export { collect } from "./collect"
+export { getGroupedObservable } from "./getGroupedObservable"
 export { createListener } from "./createListener"
 export { mergeWithKey } from "./mergeWithKey"
 export { split } from "./split"
