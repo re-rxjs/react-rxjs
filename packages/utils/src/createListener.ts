5aec029ea4dccf95041c36e5af73e45c984ab43b
Josep M Sobrepere <josepot@gmail.com>
2021-03-23 21:40:34 +0100

    feat(utils): createSignal & createKeyedSignal

--- packages/utils/src/createListener.ts
+++ packages/utils/src/createListener.ts
@@ -1,16 +1,24 @@
-import { Observable, Subject } from "rxjs"
+import { Observable } from "rxjs"
+import { createSignal } from "./createSignal"
 
-const defaultMapper: any = (v: unknown) => v
-
+/** @deprecated createListener is deprecated and it will be removed in the next version, please use createSignal. */
 export function createListener<A extends unknown[], T>(
   mapper: (...args: A) => T,
 ): [Observable<T>, (...args: A) => void]
+
+/** @deprecated createListener is deprecated and it will be removed in the next version, please use createSignal. */
 export function createListener(): [Observable<void>, () => void]
+
+/** @deprecated createListener is deprecated and it will be removed in the next version, please use createSignal. */
 export function createListener<T>(): [Observable<T>, (payload: T) => void]
 
-export function createListener<A extends unknown[], T>(
-  mapper: (...args: A) => T = defaultMapper,
-): [Observable<T>, (...args: A) => void] {
-  const subject = new Subject<T>()
-  return [subject.asObservable(), (...args: A) => subject.next(mapper(...args))]
+/**
+ * Creates a void signal. It's sugar for splitting the Observer and the Observable of a signal.
+ *
+ * @returns [1, 2]
+ * 1. The Observable
+ * 2. The emitter function.
+ */
+export function createListener(...args: any[]) {
+  return (createSignal as any)(...args)
 }
