e6ec8eef34bd72fc54b6207329d5d1d355d98112
Josep M Sobrepere <josepot@gmail.com>
2021-04-02 17:20:47 +0200

    feat(core): remove `useSubscribe`

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -1,5 +1,4 @@
 export { bind } from "./bind"
 export { shareLatest } from "./shareLatest"
 export { SUSPENSE } from "./SUSPENSE"
-export { useSubscribe } from "./useSubscribe"
 export { Subscribe } from "./Subscribe"
