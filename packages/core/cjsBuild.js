d86e1555290ea9efcc329d0d62c35600c128f6a2
Josep M Sobrepere <jm.sobrepere@gmail.com>
2022-03-29 14:13:15 +0200

    fix: React Native build

---
+++ packages/core/cjsBuild.js
@@ -0,0 +1,33 @@
+const path = require("path")
+
+const isProd = process.argv[2] === "--prod"
+
+const fixCjsPlugin = {
+  name: "fixCJS",
+  setup(build) {
+    build.onResolve({ filter: /useSyncExternalStore/ }, (args) => {
+      return {
+        path: path.join(args.resolveDir, args.path + "Cjs.ts"),
+      }
+    })
+  },
+}
+
+require("esbuild")
+  .build({
+    entryPoints: ["src/index.tsx"],
+    bundle: true,
+    outfile: isProd
+      ? "./dist/core.cjs.production.min.js"
+      : "./dist/core.cjs.development.js",
+    target: "es2015",
+    minify: isProd,
+    external: ["react", "rxjs", "use-sync-external-store"],
+    format: "cjs",
+    sourcemap: true,
+    plugins: [fixCjsPlugin],
+  })
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
