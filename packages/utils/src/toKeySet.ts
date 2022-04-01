6fe91f8a1b5aa174d628b6102a28a4226871a159
Victor Oliva <olivarra1@gmail.com>
2022-04-01 10:16:12 +0200

    feat: utils `toKeySet` (#252)
    * feat: utils `toKeySet`
    
    * Update packages/utils/src/toKeySet.ts
    
    Co-authored-by: Josep M Sobrepere <jm.sobrepere@gmail.com>
    
    * Update packages/utils/src/toKeySet.test.ts
    
    Co-authored-by: Josep M Sobrepere <jm.sobrepere@gmail.com>
    
    * organize test imports
    
    Co-authored-by: Josep M Sobrepere <jm.sobrepere@gmail.com>

---
+++ packages/utils/src/toKeySet.ts
@@ -0,0 +1,28 @@
+import { Observable, OperatorFunction } from "rxjs"
+import { KeyChanges } from "./partitionByKey"
+
+export function toKeySet<K>(): OperatorFunction<KeyChanges<K>, Set<K>> {
+  return (source$) =>
+    new Observable<Set<K>>((observer) => {
+      const result = new Set<K>()
+      let pristine = true
+      const subscription = source$.subscribe({
+        next({ type, keys }) {
+          const action = type === "add" ? type : "delete"
+          for (let k of keys) {
+            result[action](k)
+          }
+          observer.next(result)
+          pristine = false
+        },
+        error(e) {
+          observer.error(e)
+        },
+        complete() {
+          observer.complete()
+        },
+      })
+      if (pristine) observer.next(result)
+      return subscription
+    })
+}
