b194d558cafda81f6773a9d2ba28650db5234946
Victor Oliva <olivarra1@gmail.com>
2022-04-21 14:39:15 +0200

    docs: add description for `toKeySet` operator (#257)
    Co-authored-by: Josep M Sobrepere <jm.sobrepere@gmail.com>

--- packages/utils/src/toKeySet.ts
+++ packages/utils/src/toKeySet.ts
@@ -1,6 +1,10 @@
 import { Observable, OperatorFunction } from "rxjs"
 import { KeyChanges } from "./partitionByKey"
 
+/**
+ * Operator function that maps a stream of KeyChanges into a Set that contains
+ * the active keys.
+ */
 export function toKeySet<K>(): OperatorFunction<KeyChanges<K>, Set<K>> {
   return (source$) =>
     new Observable<Set<K>>((observer) => {
