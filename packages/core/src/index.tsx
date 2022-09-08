c9ac6dd764fb75343316ce6a599affe3435af1da
Víctor Oliva <olivarra1@gmail.com>
2022-09-08 17:51:33 +0200

    override all

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -1,5 +1,19 @@
-export * from "@rx-state/core"
+export {
+  AddStopArg,
+  DefaultedStateObservable,
+  EmptyObservableError,
+  NoSubscribersError,
+  PipeState,
+  SUSPENSE,
+  StateObservable,
+  StatePromise,
+  WithDefaultOperator,
+  liftSuspense,
+  sinkSuspense,
+  withDefault,
+} from "@rx-state/core"
 export { shareLatest } from "./shareLatest"
 export { useStateObservable } from "./useStateObservable"
 export { bind } from "./bind"
 export { Subscribe, RemoveSubscribe } from "./Subscribe"
+export { state } from "./stateJsx"
